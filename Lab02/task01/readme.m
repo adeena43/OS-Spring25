10d6b660c46aecd18e9e7ca01cad274f5e730dd9
Adina Faraz <adinafraz01@gmail.com>
2025-01-29 18:58:02 +0500

    Update and rename Lab02/Task01.cpp to Lab02/task01/readme.m

---
+++ Lab02/task01/readme.m
@@ -0,0 +1,3 @@
+
+![Screenshot from 2025-01-29 15-36-29](https://github.com/user-attachments/assets/5b9a13d7-eaf0-41ae-aff1-10aa67316bf9)
+![Task-1 group](https://github.com/user-attachments/assets/57c8d05e-db5e-4f24-959e-af8542a8a0c0)
