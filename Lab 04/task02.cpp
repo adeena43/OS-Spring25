47fec2456512141be2106a1133f8f546968dd912
Adina Faraz <adinafraz01@gmail.com>
2025-02-15 20:01:45 +0500

    Update task02.cpp

--- Lab 04/task02.cpp
+++ Lab 04/task02.cpp
@@ -1,1 +1,17 @@
-hhh
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        for (int i = 0; i < 100; i++)
+            printf("I am a child process\n");
+    } else {
+        for (int i = 0; i < 100; i++)
+            printf("I am a parent process\n");
+    }
+
+    return 0;
+}
